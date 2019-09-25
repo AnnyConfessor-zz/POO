96ef51d93e7dd66cd1e75562fc8e5b98d0c22a7b
annyconfessor <annyconfessor.ectufrn@gmail.com>
2019-09-25 14:13:07 -0300

    Exercício: Urnas Eletrônicas

---
+++ Urnas-eletronicas/exerciciosUrnasEletronicas.py
@@ -0,0 +1,31 @@
+#exercicioUrnasEletronicas
+
+class urna:
+    def __init__(self, endereco):
+        self.__endereco = endereco
+        votos = []
+
+class pessoa:
+    def __init__(self, nome, votoPessoa, cont):
+        self.__nome = nome
+        self.__votoPessoa = votoPessoa
+        self.__cont = cont
+
+    def votacao(self, votoPessoa, cont):
+        for i in votos:
+            print(votoPessoa)
+        cont += 1
+        if (cont > 1):
+            break
+
+class candidatos:
+    def __init__(self, id):
+        self.__id = id
+
+
+
+def main():
+    voto1 = votoPessoa('Carlos - Chapa 1')
+    
+        
+
