e62bac518e070aead79d0527cabca602ba440fab
labinfo <labinfo@ectufrn.ect.ufrn.br>
2019-10-04 12:22:31 -0300

    Lab de Herança

---
+++ lab-herança/lab-heranca.py
@@ -0,0 +1,51 @@
+class library:
+    def __init__(self, id):
+        self.id = identifier
+
+    def book(self):
+        print('Books')
+
+    def periodic(self):
+        print('Periodics')
+
+    @bstractmethod
+    def loand(self, identifier):
+        #may I put an identifier here?
+        lend = []
+        for i in lend:
+            maximum = identifier
+            print('Load done')
+
+class usuario:
+    @abstractmethod
+    def __init__(self, adress, cpf, dataNas):
+        self.__adress = adress
+        self.__cpf = cpf
+        self.__dataNas = dataNas
+
+class studant:
+    def __init__(self, curse, numRegister, ingresso):
+        self.curse = curse
+        self.__numRegister = numRegister
+        self.ingresso = ingresso
+
+    def loand(self, maxStudant):
+        #Need fix this
+        self.maxStudant = maximum
+        if lend > 3:
+            print('A studant can take until 4 books or periodics')
+            
+class teacher:
+    def __init__(self,lotacao):
+        self.lotacao = lotacao
+
+    def loand(self, maxTeacher):
+        #Need fix this
+        self.max = maximum
+        if lend > 2:
+            print('A teacher can take until 2 books or periodics')
+
+class special:
+    #Need fix this
+    def loand(self, maxSpecial):
+        self.max = maximum
